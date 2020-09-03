5db7e00d0a6bda12aef7739152a3893fa94c54e7
jashjchoi <jshjchoi@gmail.com>
2020-09-03 00:36:10 +0000

    Update task 13 and the header file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+* insert_node - inserts a number into a sorted sll
+* @head: sll
+* @number: input number
+* Return: address of the new node, or NULL if it failed
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));	
+	listint_t *prevnode = *head, *nextnode;
+
+	newnode->next = NULL;
+	newnode->n = number;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	nextnode = (*head)->next;
+	if (prevnode->n > newnode->n)
+	{
+		newnode->next = prevnode;
+		*head = newnode;
+		return (newnode);
+	}
+	while (nextnode != NULL)
+	{
+		if (prevnode->n >= newnode->n)
+		{
+			prevnode->next = newnode;
+			newnode->next = nextnode;
+			return (newnode);
+		}
+		prevnode = nextnode;
+		nextnode = nextnode->next;
+	}
+	prevnode->next = newnode;
+	return (newnode);
+}
