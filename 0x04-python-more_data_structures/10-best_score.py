eeeeeb0227fb557188b326d5d4bfdd824e58304f
jashjchoi <jshjchoi@gmail.com>
2020-09-10 20:51:00 +0000

    Fix the error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary:
         return (max(a_dictionary))
     else:
-        return None#!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary:
-        return (max(a_dictionary))
-    else:
         return None
