e8a6b42605f32e3505ef66f26c41001d7adc8d31
jashjchoi <jshjchoi@gmail.com>
2020-09-10 20:12:38 +0000

    Fix the syntax error according to pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-   return [[num, replace][num is search] for num in my_list]#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-   return [[num, replace][num is search] for num in my_list]
+    return [[num, replace][num is search] for num in my_list]
