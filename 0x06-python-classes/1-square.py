6cb7a8b8a85ef432af21210963bdf5b009dde0f8
jashjchoi <jshjchoi@gmail.com>
2020-09-22 23:46:34 +0000

    Change the documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""file for task 1"""
+
+
 class Square:
+    """define size attribute for Square Class"""
     def __init__(self, size):
+        """init"""
         self.__size = size
