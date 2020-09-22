7645dc7512580b7d107c139bcfe3933b4d70b81f
jashjchoi <jshjchoi@gmail.com>
2020-09-22 23:52:27 +0000

    Change the documentation and syntax issues

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""file for task 3"""
+
+
 class Square:
+    """define class Square with area method"""
     def __init__(self, size=0):
+        """init private instance attribute"""
         self.__size = size
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
-         return self.__size ** 2
+        """returns the current square area"""
+        return self.__size ** 2
