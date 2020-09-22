1a34b4334cd15900d0dc3060592be25ef11d80ce
jashjchoi <jshjchoi@gmail.com>
2020-09-22 23:53:16 +0000

    Change the documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""file for task 2"""
+
 class Square:
+    """define size validation for class Square"""
     def __init__(self, size=0):
+        """init private instance attribute"""
         self.__size = size
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
