c342525ec6bdb8f06fa1221921cefbc3a57f713c
jashjchoi <jshjchoi@gmail.com>
2020-09-23 18:23:26 +0000

    Fix the whitespaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,7 +18,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
-        
+
     def area(self):
         """returns area of square"""
         return self.__size ** 2
