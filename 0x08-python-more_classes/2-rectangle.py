ff0f29e4bcacb57f6f2c7a6f66a5d0abac69f537
jashjchoi <jashjchoi@gmail.com>
2020-09-29 00:40:46 +0000

    Change the function

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,20 +15,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def height(self):
-        """ getter for height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ setter for height"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
     def width(self):
         """ getter for width"""
         return self.__width
@@ -36,11 +22,25 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ setter for width"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """ getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """returns the rectangle area"""
