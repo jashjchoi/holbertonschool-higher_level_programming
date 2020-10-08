4f36e156f473d9a87437854b43aaffe222ddfb80
jashjchoi <jashjchoi@gmail.com>
2020-10-08 00:17:56 +0000

    Fix the syntax issues

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """Returns created object from a JSON file"""
 
     with open(filename, mode="r", encoding="UTF-8") as f:
-       return json.load(f) 
+        return json.load(f)
