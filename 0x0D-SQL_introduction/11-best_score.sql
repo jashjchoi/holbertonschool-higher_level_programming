6148d0d130bffcb3639d04d7f32d14412d56cd3b
jashjchoi <jashjchoi@gmail.com>
2020-11-16 19:16:51 +0000

    Add task 11~13

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- lists all records with a score >= 10 
+-- in the table second_table of the database hbtn_0c_0 in MySQL server
+-- ordered by top score first
+SELECT score, name FROM second_table HAVING score >= 10 ORDER BY score DESC;
