c9b504f3d1dba7ab7f18ee600d33910d0f2c0124
jashjchoi <jashjchoi@gmail.com>
2020-11-17 22:56:45 +0000

    Add task 3~5

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates the table force_name on MySQL server
+-- If the table force_name already exists, script should not fail
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- creates the table id_not_null on MySQL server 
+-- If the table id_not_null already exists, script should not fail
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
