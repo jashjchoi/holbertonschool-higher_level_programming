7b4ae89d991e07007432c3672830319b20990a9c
jashjchoi <jashjchoi@gmail.com>
2020-12-12 21:17:27 +0000

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Displays name argument of states table"""
+import MySQLdb
+import sys
+
+
+def filter_names():
+    """takes 4 arguments argv to list from db
+    Only lists with states that matches the argument
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+        argv[4]: state name searched
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    """creates a cursor and executes query"""
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states\
+                WHERE BINARY name='{:s}'\
+                ORDER BY id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    """closes cursor and db"""
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_names()
