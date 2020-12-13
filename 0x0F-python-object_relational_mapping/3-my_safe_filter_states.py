638052b88d6c75e13f98b0519e75a5612ebd99dc
jashjchoi <jashjchoi@gmail.com>
2020-12-13 08:44:23 +0000

    Remove the else statement and if condition

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 import sys
 
 
-def main():
+def safe_st_filter():
     """takes 4 arguments argv to list from db
     Only lists with states that matches the argument
         argv[1]: mysql username
@@ -13,29 +13,23 @@ def main():
         argv[3]: database name
         argv[4]: state name searched
     """
-    if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-        """creates a cursor and executes query"""
-        cur = db.cursor()
-
-        cur.execute("SELECT * FROM states\
-                    WHERE BINARY name='{:s}'\
-                    ORDER BY id ASC".format(sys.argv[4]))
-        rows = cur.fetchall()
-        for i in rows:
-            print(i)
-
-        """closes cursor and db"""
-        cur.close()
-        db.close()
-    else:
-        return
+    """creates a cursor and executes query"""
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                   WHERE BINARY name=%s
+                   ORDER BY id ASC""", (sys.argv[4],))
+    for i in cur.fetchall():
+        print(i)
+    """closes cursor and db"""
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    main()
+    safe_st_filter()
