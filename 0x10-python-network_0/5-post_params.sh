ed1c4efbdf702bd81ad7ed6ef6b3bcbee5bf4b8f
jashjchoi <jashjchoi@gmail.com>
2020-12-18 21:20:07 +0000

    Debug the errors

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL sends a POST request to the passed URL
 # and displays the body of the response
-curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
