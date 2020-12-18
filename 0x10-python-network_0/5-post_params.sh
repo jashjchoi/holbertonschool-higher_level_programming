5ba415dd6384efb63fc7feb20b13414ee90947f9
jashjchoi <jashjchoi@gmail.com>
2020-12-18 21:14:35 +0000

    add more flags

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL sends a POST request to the passed URL
 # and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
