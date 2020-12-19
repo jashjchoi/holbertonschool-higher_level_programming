df13e566124a924e324fd100d3a18e4190c364cc
jashjchoi <jashjchoi@gmail.com>
2020-12-19 01:48:52 +0000

    delete the flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL sends a POST request and displays the body of the response
-curl -sX POST -L -d "email=hr@holbertonschool.com&subject=I will always be there for PLD" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be there for PLD" "$1"
