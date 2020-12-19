0c830615d73fa71c6bfa64149172c0cc03251ad5
jashjchoi <jashjchoi@gmail.com>
2020-12-19 01:45:58 +0000

    Delete comments

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL sends a POST request to the passed URL
-# and displays the body of the response
+# takes in a URL sends a POST request and displays the body of the response
 curl -sX POST -L -d "email=hr@holbertonschool.com&subject=I will always be there for PLD" "$1"
