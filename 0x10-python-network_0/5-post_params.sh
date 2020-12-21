fa5f82c99bc6f1d8bbb81f26cae3c939b9187a57
jashjchoi <jashjchoi@gmail.com>
2020-12-21 21:11:11 +0000

    Fix the typo error

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL sends a POST request and displays the body of the response
-curl -s -X POST -d 'email=hr@holbertonschool.com' -d 'subject=I will always be there for PLD' "$1"
+# takes in a URL sends a POST request and displays the response
+curl -sX POST -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' "$1"
