7f57c6c0ee6c4f73bd040438f454680d3e209772
jashjchoi <jashjchoi@gmail.com>
2021-01-12 03:04:34 +0000

    Add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+takes Github credentials (username and pw) 
+and uses the Github API to display your id
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    usrname = argv[1]
+    pw = argv[2]
+    req = requests.get("https://api.github.com/user", auth=(usrname, pw))
+    print(req.json().get("id"))
