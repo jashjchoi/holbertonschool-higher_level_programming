d54c8c7ab540615879dba9f8ea955f77499033da
jashjchoi <jashjchoi@gmail.com>
2021-02-02 17:48:17 +0000

    Add task 6 and 7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+  print 3 lines with loop function
+*/
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of list) {
+  console.log(line);
+}
