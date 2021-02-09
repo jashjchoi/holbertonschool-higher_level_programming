050220158d540c967fb2d1dccf332c235aff68bb
jashjchoi <jashjchoi@gmail.com>
2021-02-09 23:30:42 +0000

    Fix the syntax errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf-8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
