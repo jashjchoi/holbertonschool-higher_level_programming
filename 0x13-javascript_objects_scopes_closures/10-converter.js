3abde30dc4926ef0d097bdadf07f1a85ca06ffe5
jashjchoi <jashjchoi@gmail.com>
2021-03-21 23:20:07 +0000

    0x13-java folder deleted

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// converts a number from base 10 to another base
-exports.converter = function (base) {
-  return function (number) {
-    return number.toString(base);
-  };
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// prints the number of arguments already printed
-// and the new argument value
-
-let n = 0;
-exports.logMe = function (item) {
-  console.log(n + ': ' + item);
-  n++;
-};
