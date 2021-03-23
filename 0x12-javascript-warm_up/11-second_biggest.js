848d9f73ffbcf57192b98be9398411b8b0fce6c6
jashjchoi <jashjchoi@gmail.com>
2021-03-23 02:25:08 +0000

    Fix the syntax errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,14 @@
   searches the second biggest integer in the list of arguments
 */
 
-if (!process.argv[2] || !process.argv[3])
-{
+if (!process.argv[2] || !process.argv[3]) {
   console.log('0');
 } else {
-    let list = [];
-    for (let i = 0; process.argv[i]; i++) {
-        list.push(process.argv[i]);
-    }
+  const list = [];
+  for (let i = 0; process.argv[i]; i++) {
+    list.push(process.argv[i]);
+  }
   list.sort();
-    let index = list.length - 2;
-    console.log(list[index]);
+  const index = list.length - 2;
+  console.log(list[index]);
 }
