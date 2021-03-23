1ccab84cd9b97b9b98b0980eb769e2fb07c0d6b4
jashjchoi <jashjchoi@gmail.com>
2021-03-23 02:00:39 +0000

    Add task 13 files

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+  prints the addition of 2 integers
+  Function must be visible from outside (13-main.js)
+  Name of the function must be add
+*/
+
+exports.add = function (a, b) {
+  return (a + b);
+};
