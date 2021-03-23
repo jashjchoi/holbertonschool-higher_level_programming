feceb3501da689ce837ff00d5b8ad35cdfffb46b
jashjchoi <jashjchoi@gmail.com>
2021-03-23 02:45:15 +0000

    Fix the output error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,10 @@
   searches the second biggest integer in the list of arguments
 */
 
-if (!process.argv[2] || !process.argv[3] || process.argv[1] === 1) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const list = [];
-  for (let i = 0; process.argv[i]; i++) {
-    list.push(process.argv[i]);
-  }
-  list.sort();
-  const index = list.length - 2;
-  console.log(list[index]);
+  const idx = process.argv.slice(2);
+  idx.sort(function (a, b) { return b - a; });
+  console.log(idx[1]);
 }
