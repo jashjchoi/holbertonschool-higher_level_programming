16bf5df8085aa63f39ac78612b93a96ab81eb413
jashjchoi <jashjchoi@gmail.com>
2021-03-23 23:31:26 +0000

    Add task 4, 5 files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// class Square that defines a square
+const mySquare = require('./5-square');
+
+class Square extends mySquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
