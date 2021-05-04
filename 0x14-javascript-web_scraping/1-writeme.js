f11d539ed767146ff7bfe03c3765854731e38811
jashjchoi <jashjchoi@gmail.com>
2021-05-04 21:44:07 +0000

    Add task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// reads and prints the content of a file
+const fs = require('fs');
+const fileName = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(fileName, text, 'utf-8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
