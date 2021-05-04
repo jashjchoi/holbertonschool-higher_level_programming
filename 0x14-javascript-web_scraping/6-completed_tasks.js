4e2b0b6bc686eee4b5cd72b872b3274f9daaf1f0
jashjchoi <jashjchoi@gmail.com>
2021-05-04 22:27:50 +0000

    Add task 6 file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const jsonBody = JSON.parse(body);
+    const dictCompletTodoUser = {};
+    for (const todo of jsonBody) {
+      if (todo.completed) {
+        if (dictCompletTodoUser[todo.userId]) {
+          dictCompletTodoUser[todo.userId] += 1;
+        } else {
+          dictCompletTodoUser[todo.userId] = 1;
+        }
+      }
+    }
+    console.log(dictCompletTodoUser);
+  }
+});
