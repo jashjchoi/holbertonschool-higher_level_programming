41dadb564911b2ad747ef4ec7a11422d33db20ea
jashjchoi <jashjchoi@gmail.com>
2021-05-04 21:39:21 +0000

    Set up the new project folder and add files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// reads and prints the content of a file
+const fs = require('fs');
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
